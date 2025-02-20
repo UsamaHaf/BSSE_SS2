244c1e9263d8bd2c8865ef00c8c6bc26b5e9297b
mhmd.usama.hafeez <mhmd.usama.hafeez@gmail.com>
2025-02-20 16:14:35 +0500

    Form Validation

--- app/src/main/java/com/usama/uos/bssess2/SignupActivity.kt
+++ app/src/main/java/com/usama/uos/bssess2/SignupActivity.kt
@@ -1,25 +1,21 @@
 package com.usama.uos.bssess2
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SignupActivity : AppCompatActivity() {
 
-   private lateinit var emailAddress:EditText
-   private lateinit var phoneNumber:EditText
-   private lateinit var lastName:EditText
-   private lateinit var firstName:EditText
-   private lateinit var password:EditText
-   private lateinit var signup:Button
+   private lateinit var emailAddress: EditText
+   private lateinit var phoneNumber: EditText
+   private lateinit var lastName: EditText
+   private lateinit var firstName: EditText
+   private lateinit var password: EditText
+   private lateinit var signup: Button
 
-   @SuppressLint("MissingInflatedId")
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_signup)
@@ -33,14 +29,41 @@ class SignupActivity : AppCompatActivity() {
 
       signup.setOnClickListener {
 
-         Toast.makeText(this@SignupActivity, "Signup Checking Listener" , Toast.LENGTH_SHORT).show()
+         val strEmail = emailAddress.text.toString()
+         val strPhoneNumber = phoneNumber.text.toString()
+         val strFName = firstName.text.toString()
+         val strLastName = lastName.text.toString()
+         val strPassword = password.text.toString()
+
+         signUpUser(strEmail, strPhoneNumber, strFName, strLastName, strPassword)
 
       }
 
 
    }
-   fun signup(){
 
+   private fun signUpUser(strEmail: String, strPhoneNumber: String, strFName: String, strLastName: String, strPassword: String) {
+      if (strEmail.isEmpty()) {
+         Toast.makeText(this@SignupActivity, "Enter Email Address", Toast.LENGTH_SHORT).show()
 
+      } else if (strPhoneNumber.isEmpty()) {
+         Toast.makeText(this@SignupActivity, "Enter Phone Number", Toast.LENGTH_SHORT).show()
+
+      } else if (strFName.isEmpty()) {
+         Toast.makeText(this@SignupActivity, "Enter First Name", Toast.LENGTH_SHORT).show()
+
+      } else if (strLastName.isEmpty()) {
+         Toast.makeText(this@SignupActivity, "Enter Last Name", Toast.LENGTH_SHORT).show()
+
+      } else if (strPassword.isEmpty() && strPassword.equals("1232e")) {
+         Toast.makeText(this@SignupActivity, "Enter Password", Toast.LENGTH_SHORT).show()
+
+      } else {
+         Toast.makeText(this@SignupActivity, "SignUp Successful BSSE SS2", Toast.LENGTH_LONG).show()
+
+         startActivity(Intent(this@SignupActivity , LoginActivity::class.java))
+
+      }
    }
+
 }
